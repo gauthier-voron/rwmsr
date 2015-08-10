ab08fba34e7f8add0c7b24f02cdc0cc2e25e061f
Gauthier Voron <gauthier.voron@lip6.fr>
2015-08-10 16:50:19 +0200

    project: add license GPL3
    Add the GPL3 license in COPYING and the copyright in every source
    files.

--- xen-tokyo/hypercall.c
+++ xen-tokyo/hypercall.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2015 Gauthier Voron
+ * This file is part of rwmsr.
+ *
+ * Rwmsr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rwmsr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rwmsr. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <alloca.h>
 #include <stdint.h>
 #include <string.h>
